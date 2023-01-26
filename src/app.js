75343a31ba31b71507b5c30214d1fb0e1ddd4bb2
Lizeth Gutierrez <lizethd.gutierrez11@gmail.com>
2023-01-26 00:07:50 -0600

    coxion a base de datos

--- src/app.js
+++ src/app.js
@@ -1,11 +1,23 @@
 const express = require('express');
+const { Sequelize } = require('sequelize');
 
 const app = express();
 const PORT = 8000;
 
+const sequelize = new Sequelize('postgresql://postgres:ruut@localhost:5432/entregable3');
+
+try {
+    sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+} catch (error) {
+    console.error('Unable to connect to the database:', error);
+}
+
 app.get('/', (req, res) => {
     res.status(200).json({ message: "Welcome to the server"})
 });
+
+// definir endpoints
 
 app.listen(PORT, () => {
     console.log(`Server up and running. Listening on port ${PORT}.`);
